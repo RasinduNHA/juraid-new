5226101dd37d4a8a65347a65b12a4804e7241307
Rasindu <it22092410@my.sliit.lk>
2026-04-26 12:44:37 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="shell footer-content">
-        <p>SLIIT Research Project | JuriAid Legal-Tech Initiative</p>
+        <p>JuriAid Legal-Tech Initiative</p>
         <div className="social-links">
           <a href="#">LinkedIn</a>
-          <a href="#">GitHub</a>
+          <a href="https://github.com/ChathuminaK/JuriAid_App_Backend">GitHub</a>
           <a href="#">YouTube</a>
         </div>
         <p>Copyright {new Date().getFullYear()} JuriAid. All rights reserved.</p>
